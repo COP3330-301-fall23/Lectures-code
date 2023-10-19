64edcae55d44d5ab6bf82cb41ffa8c9176357298
CarlosTheran <carlos.theran@upr.edu>
2023-10-19 14:12:27 -0400

    Oct-19-301

---
+++ Unit9/implementation.cpp
@@ -0,0 +1,28 @@
+#include "Students.h";
+#include <fstream>
+#include <iostream>
+
+Students::Students(ifstream &InputFile, string file_name)
+{
+    int index = 0;
+    InputFile.open(file_name);
+
+    if(!InputFile)
+    {
+        cout<<"Error: the path or file does not exist";
+        exit(1);
+    }
+
+
+
+    while(!InputFile.eof())
+    {
+        InputFile.ignore(100,' ');
+        for(int j=0; j<3; j++)
+        {
+            InputFile >> grades[index][j];
+        }
+        index++;
+    }
+    InputFile.close();
+}
